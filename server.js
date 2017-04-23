9a38d84c8de44f567977376fc108e93dedeed87a
rodrigo figueroa <ruimonroy@csu.fullerton.edu>
2017-04-22 20:03:07 -0700

    added method to upload a file

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 //include requirement
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(fileUpload());
 
 app.use('/', express.static('public'));
 
-
+// simple get method
 app.get('/testGet', function(req, res){
   'use strict';
-  console.log('score');
   res.json({'this':'is A test'});
 
 });
 
-
-app.post('/testPost', function(req, res){
-  'use strict';
-
-      console.log('Data');
-      res.json('send some DATA!');
+// post image to server and save it as filename.jpg
+app.post('/upload', function(req, res) {
+	console.log(req.files);
+  if (!req.files)
+  {
+  	console.log("no file");
+    return res.status(400).send('No files were uploaded.');
+ 	}
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
+  let sampleFile = req.files.sampleFile;
+ 
+  // Use the mv() method to place the file somewhere on your server 
+  sampleFile.mv('./imgs/filename.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+ 
+    res.send('File uploaded!');
+  });
 });
 
-app.post('/testPost', function(req, res){
-  'use strict';
 
-      console.log('Data');
-      res.json('send some DATA!');
-});
 
 app.listen(port,'0.0.0.0',function(){console.log('!---- AWESOME SAUCE listening in '+ port+' ----!');});
