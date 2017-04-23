03663da265609b2fc37b453796f2eb29bb7384d2
rodrigo figueroa <ruimonroy@csu.fullerton.edu>
2017-04-23 04:31:22 -0700

    many imgs

--- server.js
+++ server.js
@@ -29,14 +29,15 @@ app.post('/upload', function(req, res) {
   	console.log("no file");
     return res.status(400).send('No files were uploaded.');
  	}
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   var sampleFile = req.files.sampleFile;
- 
-  // Use the mv() method to place the file somewhere on your server 
-  sampleFile.mv('./imgs/filename.jpg', function(err) {
+
+  // Use the mv() method to place the file somewhere on your server
+
+  sampleFile.mv('./imgs/'+name, function(err) {
     if (err)
       return res.status(500).send(err);
- 
+
     res.send('File uploaded!');
   });
 });
