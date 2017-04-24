e7e3a4274572beab14248a0e24b9abb58fa0a2df
rodrigo figueroa <ruimonroy@csu.fullerton.edu>
2017-04-23 23:30:14 -0700

    fixed call back but still no results

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 var PythonShell = require('python-shell');
 
 var options = {
-  mode: 'text',
+  // mode: 'text',
   // pythonPath: './',
   // pythonOptions: ['-u'],
   // scriptPath: 'path/to/my/scripts',
@@ -37,28 +37,28 @@ app.get('/testGet', function(req, res){
 // post image to server and save it as filename.jpg
 app.post('/upload', function(req, res) {
 	// console.log("hey");
-  if (!req.files)
-  {
-  	console.log("no file");
-    return res.status(400).send('No files were uploaded.');
- 	}
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  var sampleFile = req.files.sampleFile;
-
-  // Use the mv() method to place the file somewhere on your server
-
-  sampleFile.mv('./imgs/filename.jpg' , function(err) {
-    if (err)
-    {
-      return res.status(500).send(err);
-
-        }
+  // if (!req.files)
+  // {
+  // 	console.log("no file");
+  //   return res.status(400).send('No files were uploaded.');
+  // }
+  // // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  // var sampleFile = req.files.sampleFile;
+  //
+  // // Use the mv() method to place the file somewhere on your server
+  //
+  // sampleFile.mv('./imgs/filename.jpg' , function(err) {
+  //   if (err)
+  //   {
+  //     return res.status(500).send(err);
+  //
+  //       }
 
         console.log("about to");
 
         PythonShell.run('animal_classify.py', options, function (err, results) {
              console.log("in");
-            if (err) console.log(err);
+            // if (err) console.log(err);
             // results is an array consisting of messages collected during execution
             console.log('results: %j', results);
             res.send('File uploaded!');
@@ -73,7 +73,7 @@ app.post('/upload', function(req, res) {
         // });
 
 
-  });
+  // });
 });
 
 
